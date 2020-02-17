b8ede63fbdd75d2ab585499a0c03dc7b768d422a
HongHyukJin <gurwlszx@naver.com>
2020-02-17 14:36:29 +0900

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gurwl\onedrive\desktop\hyukjin\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
